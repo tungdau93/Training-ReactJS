bc2630aa8b0329d1dbcac37f5ffc1bf075db4ec3
KhoapdMor <khoapd@mor.com.vn>
2022-09-22 17:33:05 +0700

    searchwhencallAPI

--- src/Assignments/BaiTap2.jsx
+++ src/Assignments/BaiTap2.jsx
@@ -1,14 +1,13 @@
 // import {Link} from 'react-router-dom'
 import React from "react";
 import "../style/_bai-tap-2.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  useEffect } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 
-
-// const clickInput = document.querySelector(
+// const clickInput = document.getElementsByClassName(
 //   ".app__container--input-wrapper--input"
 // );
-// const showList = document.querySelector(
+// const showList = document.getElementsByClassName(
 //   ".app__container--input-wrapper--dropdown-list"
 // );
 
@@ -18,28 +17,59 @@ import {  useEffect } from "react";
 //   }
 // };
 
-// // const hideListItem = (e) => {
-// //   if (!showList.contains(e.target)) {
-// //     showList.style.display = "none";
-// //   }
-// // };
+// const hideListItem = (e) => {
+//   if (!showList.contains(e.target)) {
+//     showList.style.display = "none";
+//   }
+// };
 
 const BaiTap2 = () => {
 
-  useEffect(()=>{
+  
+  const [cities, setCities] = useState([]);
+  // const [value,setValue] = useState("");
+  const [cityMatch, setCityMatch] = useState([]);
+  const [showSuggestions, setSuggestions] = useState(false);
+
+  const handleClickSuggestion =()=>{
+    // document.getElementsByClassName("selection-queue").innerText = "ha noi"
+    setSuggestions(!showSuggestions)
+
+  }
+
+  useEffect(() => {
     fetch("https://provinces.open-api.vn/api/")
-    .then(response => response.json())
-    .then(data =>console.log(data))
-      
-    },[])
+      .then((response) => response.json())
+      .then((data) => setCities(data));
+    // console.log(cities)
+  }, []);
 
-  const onHoverItemIn = (e) => {
-    e.target.style.backgroundColor = "#617D98";
+  const searchCities = (text) => {
+    // if (document.getElementsByClassName("app__container--input-wrapper--input").value === ""){
+    //   document.getElementsByClassName("app__container--input-wrapper--dropdown-list--item1").display ="none"
+    // }
+    let matches = cities.filter((city) => {
+      // document.getElementsByClassName("app__container--input-wrapper--dropdown-list--item1").display ="block"
+      const regex = new RegExp(`${text}`, "gi");
+      return city.name.match(regex);
+    });
+    setCityMatch(matches);
+    
+  //  return null
   };
 
-  const onHoverItemOut = (e) => {
-    e.target.style.backgroundColor = "rgba(230, 249, 255, 0.2)";
-  };
+  // const onHoverItemIn = (e) => {
+  //   e.target.style.backgroundColor = "#617D98";
+  // };
+
+  // const handleChangeValue = (e) => {
+  //   setValue(e.target.value);
+  //   // showList.style.display = "none";
+  //   console.log(value)
+  // }
+  // const onHoverItemOut = (e) => {
+  //   e.target.style.backgroundColor = "rgba(230, 249, 255, 0.2)";
+  // };
 
   return (
     <div>
@@ -56,31 +86,30 @@ const BaiTap2 = () => {
             </div>
           </div>
           <div className="app__container--input-wrapper">
-            <span>
-              <FontAwesomeIcon icon="fa-thin fa-magnifying-glass" />
-            </span>
+            <img className="finding-icon" src={require('../asserts/images/search.png')}/>
             <input
+              // value="" 
+              onChange={(e) => searchCities(e.target.value)}
               type="text"
               placeholder="Nhập tên thành phố để tìm kiếm..."
               className="app__container--input-wrapper--input"
             ></input>
-            <div className="app__container--input-wrapper--dropdown-list">
-              <div
-                className="app__container--input-wrapper--dropdown-list--item1"
-                onMouseOver={onHoverItemIn}
-                onMouseOut={onHoverItemOut}
-              ></div>
-              <div
-                className="app__container--input-wrapper--dropdown-list--item2"
-                onMouseOver={onHoverItemIn}
-                onMouseOut={onHoverItemOut}
-              ></div>
-              <div
-                className="app__container--input-wrapper--dropdown-list--item3"
-                onMouseOver={onHoverItemIn}
-                onMouseOut={onHoverItemOut}
-              ></div>
-            </div>
+            <div style={{overflow:"auto"}} className="app__container--input-wrapper--dropdown-list--item1">
+              {cityMatch &&
+                cityMatch.slice(0,3).map((city) => {
+                  // console.log(cities)
+                  return (
+                    <>
+                      <div onClick ={handleClickSuggestion} className ="suggestion" key={city.code}>
+                        <>{city.name}</>
+                      </div>
+                    </>
+                  );
+                })}
+            </div >
+          {showSuggestions?<div className="selection-queue">
+             {cityMatch.map(city => <div key ={city.code}>{city.name}</div>).slice(0,1)}
+          </div>:<></>}
           </div>
         </div>
       </div>
