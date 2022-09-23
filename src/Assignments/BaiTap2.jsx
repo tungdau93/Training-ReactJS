2853319699742b41f151c4900b5c89a9276f55c4
KhoapdMor <khoapd@mor.com.vn>
2022-09-23 17:28:25 +0700

    select, show cities

--- src/Assignments/BaiTap2.jsx
+++ src/Assignments/BaiTap2.jsx
@@ -1,75 +1,46 @@
-// import {Link} from 'react-router-dom'
 import React from "react";
 import "../style/_bai-tap-2.scss";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 
-// const clickInput = document.getElementsByClassName(
-//   ".app__container--input-wrapper--input"
-// );
-// const showList = document.getElementsByClassName(
-//   ".app__container--input-wrapper--dropdown-list"
-// );
-
-// const showListItem = (e) => {
-//   if (!clickInput.contains(e.target)) {
-//     showList.style.display = "block";
-//   }
-// };
-
-// const hideListItem = (e) => {
-//   if (!showList.contains(e.target)) {
-//     showList.style.display = "none";
-//   }
-// };
-
 const BaiTap2 = () => {
-
-  
   const [cities, setCities] = useState([]);
-  // const [value,setValue] = useState("");
-  const [cityMatch, setCityMatch] = useState([]);
-  const [showSuggestions, setSuggestions] = useState(false);
+  const [citySearch, setCitySearch] = useState([]);
+  const [citiesTag, setCitiesTag] = useState([]);
 
-  const handleClickSuggestion =()=>{
-    // document.getElementsByClassName("selection-queue").innerText = "ha noi"
-    setSuggestions(!showSuggestions)
 
-  }
+  const handleSwitch = (code) => {
+    // step 1: get value of item
+    const citySelected = citySearch.find((city) => city.code === code);
+    const newCityMatch = citySearch.filter((city) => city.code !== code);
+    // console.log(newCityMatch)
+    setCitySearch([...newCityMatch]);
+
+    const newCitiesTag = [...citiesTag];
+    // console.log(newCitiesTag)
+    newCitiesTag.push(citySelected);
+    setCitiesTag([...newCitiesTag]);
+    console.log(newCitiesTag);
+  };
+
+  // step 2: delete item from cities list
+
+  // step 3: add item into suggestion list
 
   useEffect(() => {
-    fetch("https://provinces.open-api.vn/api/")
+    const url = "https://provinces.open-api.vn/api/";
+    fetch(url)
       .then((response) => response.json())
       .then((data) => setCities(data));
-    // console.log(cities)
   }, []);
 
   const searchCities = (text) => {
-    // if (document.getElementsByClassName("app__container--input-wrapper--input").value === ""){
-    //   document.getElementsByClassName("app__container--input-wrapper--dropdown-list--item1").display ="none"
-    // }
     let matches = cities.filter((city) => {
-      // document.getElementsByClassName("app__container--input-wrapper--dropdown-list--item1").display ="block"
       const regex = new RegExp(`${text}`, "gi");
       return city.name.match(regex);
     });
-    setCityMatch(matches);
-    
-  //  return null
+    setCitySearch(matches);
+    // console.log(citySearch)
   };
-
-  // const onHoverItemIn = (e) => {
-  //   e.target.style.backgroundColor = "#617D98";
-  // };
-
-  // const handleChangeValue = (e) => {
-  //   setValue(e.target.value);
-  //   // showList.style.display = "none";
-  //   console.log(value)
-  // }
-  // const onHoverItemOut = (e) => {
-  //   e.target.style.backgroundColor = "rgba(230, 249, 255, 0.2)";
-  // };
 
   return (
     <div>
@@ -86,30 +57,58 @@ const BaiTap2 = () => {
             </div>
           </div>
           <div className="app__container--input-wrapper">
-            <img className="finding-icon" src={require('../asserts/images/search.png')}/>
+            {citiesTag &&
+              citiesTag.map((cityTag) => {
+                return (
+                  <>
+                    <div
+                      key={cityTag.code}
+                      className="app__container--input-wrapper--show--cities"
+                    >
+                      <img src={require("../asserts/images/X.png")} 
+                        alt=""
+                        className="close-button"
+                        onClick={}
+                        >
+                      </img>
+                      {cityTag.name}
+                    </div>
+                  </>
+                );
+              })}
+            <img
+              className="finding-icon"
+              src={require("../asserts/images/search.png")}
+              alt=""
+            />
             <input
-              // value="" 
+              // value=""
+
               onChange={(e) => searchCities(e.target.value)}
               type="text"
               placeholder="Nhập tên thành phố để tìm kiếm..."
               className="app__container--input-wrapper--input"
             ></input>
-            <div style={{overflow:"auto"}} className="app__container--input-wrapper--dropdown-list--item1">
-              {cityMatch &&
-                cityMatch.slice(0,3).map((city) => {
-                  // console.log(cities)
+            <div
+              style={{ overflow: "auto" }}
+              className="app__container--input-wrapper--dropdown-list--item1"
+            >
+              
+              {citySearch &&
+                citySearch.map((city) => {
                   return (
                     <>
-                      <div onClick ={handleClickSuggestion} className ="suggestion" key={city.code}>
-                        <>{city.name}</>
+                      <div
+                        onClick={() => handleSwitch(city.code)}
+                        className="suggestion"
+                        key={city.code}
+                      >
+                        {city.name}
                       </div>
                     </>
                   );
                 })}
-            </div >
-          {showSuggestions?<div className="selection-queue">
-             {cityMatch.map(city => <div key ={city.code}>{city.name}</div>).slice(0,1)}
-          </div>:<></>}
+            </div>
           </div>
         </div>
       </div>
